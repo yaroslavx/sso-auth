749a1aed14f61b277718bf1df776d6a02c888529
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-12-08 21:48:33 +0300

    start service layer

---
+++ internal/services/auth/auth.go
@@ -0,0 +1,63 @@
+package auth
+
+import (
+	"context"
+	"log/slog"
+	"sso/internal/domain/models"
+	"time"
+)
+
+type Auth struct {
+	log         *slog.Logger
+	usrSave     UserSaver
+	usrProvider UserProvider
+	appProvider AppProvider
+	tokenTTL    time.Duration
+}
+
+type UserSaver interface {
+	SaveUser(ctx context.Context, email string, passHash []byte) (uid int64, err error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email string) (models.User, error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
+
+type AppProvider interface {
+	App(ctx context.Context, appID int) (models.App, error)
+}
+
+// New returns a new instance of the Auth service.
+func New(log *slog.Logger,
+	userSaver UserSaver,
+	userProvider UserProvider,
+	appProvider AppProvider,
+	tokenTTL time.Duration) *Auth {
+	return &Auth{
+		usrSave:     userSaver,
+		usrProvider: userProvider,
+		log:         log,
+		appProvider: appProvider,
+		tokenTTL:    tokenTTL,
+	}
+}
+
+// Login checks if user with given credentials exists in the system and returns access token.
+//
+// If user exists, but password is incorrect, returns error.
+// If user doesn't exist, returns error.
+func (a *Auth) Login(ctx context.Context, email string, pass string, appID int) (string, error) {
+	panic("not implemented")
+}
+
+// RegisterNewUser registers new user in the system and returns user ID.
+// If user with given username already exists, returns error.
+func (a *Auth) RegisterNewUser(ctx context.Context, email string, pass string) (int64, error) {
+	panic("not implemented")
+}
+
+// IsAdmin checks if user is admin.
+func (a *Auth) IsAdmin(ctx context.Context, userID int64) (bool, error) {
+	panic("not implemented")
+}
