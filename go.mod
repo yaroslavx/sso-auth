b7a84e985aa00a2979ce4d9ecf03c158296aecda
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-12-09 13:19:02 +0300

    implement register in service layer

--- go.mod
+++ go.mod
@@ -12,9 +12,10 @@ require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
 	golang.org/x/net v0.14.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -2,9 +2,12 @@ package auth
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"sso/internal/domain/models"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Auth struct {
@@ -54,7 +57,29 @@ func (a *Auth) Login(ctx context.Context, email string, pass string, appID int) 
 // RegisterNewUser registers new user in the system and returns user ID.
 // If user with given username already exists, returns error.
 func (a *Auth) RegisterNewUser(ctx context.Context, email string, pass string) (int64, error) {
-	panic("not implemented")
+	const op = "auth.RegisterNewUser"
+
+	log := a.log.With(slog.String("op", op))
+
+	log.Info("registering user")
+
+	passHash, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+	if err != nil {
+		log.Error("failed to generate password hash")
+
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := a.usrSave.SaveUser(ctx, email, passHash)
+	if err != nil {
+		log.Error("failed to save user")
+
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	log.Info("user registered")
+
+	return id, nil
 }
 
 // IsAdmin checks if user is admin.
